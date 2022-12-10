efa834d062878a0c45735bdde3279020c01baf1f
재우 <jaewoo98@kakao.com>
2022-12-11 00:44:36 +0900

    add Ui

--- app/src/main/java/com/course/msp/ui/slideshow/SlideshowViewModel.java
+++ app/src/main/java/com/course/msp/ui/slideshow/SlideshowViewModel.java
@@ -10,7 +10,7 @@ public class SlideshowViewModel extends ViewModel {
 
     public SlideshowViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("날짜별 보기 로딩중~");
     }
 
     public LiveData<String> getText() {
