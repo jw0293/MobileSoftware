2027956cfcea01e10f8ac3b4433652867b261626
재우 <jaewoo98@kakao.com>
2022-12-12 18:51:53 +0900

    home Image upload

--- app/src/main/java/com/course/msp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/course/msp/ui/home/HomeViewModel.java
@@ -4,7 +4,17 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.course.msp.repository.FoodInformationRepository;
+
 public class HomeViewModel extends ViewModel {
+    private MutableLiveData<String> mImg;
 
+    public HomeViewModel(){
+        mImg = new MutableLiveData<>();
+        mImg.setValue(String.valueOf(FoodInformationRepository.getLastImage()));
+    }
 
+    public LiveData<String> getLiveData() {
+        return mImg;
+    }
 }
